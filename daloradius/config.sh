ad48040d738b64cd4ac4578c81e24817aee8c754
fevenor <fevenor@outlook.com>
2015-12-22 21:12:49 -0500

    fixed database name error

--- daloradius/config.sh
+++ daloradius/config.sh
@@ -3,5 +3,5 @@ sed -i -e "s/host_temp/$DB_HOST_VALUE/" \
  -e "s/port_temp/$DB_PORT_VALUE/" \
  -e "s/user_temp/$DB_USER_VALUE/" \
  -e "s/pass_temp/$DB_PASS_VALUE/" \
- -e "s/name_temp/DB_NAME_VALUE/" \
+ -e "s/name_temp/$DB_NAME_VALUE/" \
  /var/www/localhost/htdocs/library/daloradius.conf.php
